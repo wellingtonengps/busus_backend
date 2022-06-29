d102aa10b43c40ad71b6596801c4840da2a99630
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2022-06-29 17:16:48 -0300

    feat: return token in the authentication with jwt

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import { AppDataSource } from "./data-source";
 import { User } from "./entity/User";
 import cors from "cors"
-import multer from "multer"; 
-import {v4 as uuidv4} from "uuid"
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid"
 import bcrypt from 'bcrypt';
+import * as jwt from "jsonwebtoken"
 
 const app = express();
 const port = 3000;
@@ -19,20 +20,20 @@ AppDataSource.initialize()
   .catch((error) => console.error("Error during initialization: ", error));
 
 
-const storage =  multer.diskStorage({
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-      cb(null, 'src/images/')
+    cb(null, 'src/images/')
   },
   filename: function (req, file, cb) {
-      const fileExtension = file.originalname.split('.')[1];
+    const fileExtension = file.originalname.split('.')[1];
 
-      const filename = uuidv4();
+    const filename = uuidv4();
 
-      cb(null, `${filename}.${fileExtension}`)
+    cb(null, `${filename}.${fileExtension}`)
   }
 });
 
-const upload =  multer({ storage });
+const upload = multer({ storage });
 
 
 app.get("/", (_, res) => {
@@ -42,8 +43,8 @@ app.get("/", (_, res) => {
 });
 
 
-app.post("/profileUpload", upload.single("image") ,async (req, res) => {
-    res.json(req.file?.path);
+app.post("/profileUpload", upload.single("image"), async (req, res) => {
+  res.json(req.file?.path);
 });
 
 app.get("/users", async (_, res) => {
@@ -53,7 +54,7 @@ app.get("/users", async (_, res) => {
 
 app.post("/user", upload.single("image"), async (req, res) => {
 
-  const {name, CPF, sus_code, phone_number, password } = req.body;
+  const { name, CPF, sus_code, phone_number, password } = req.body;
   const pathImage = req.file?.path;
   console.log(pathImage);
 
@@ -68,10 +69,35 @@ app.post("/user", upload.single("image"), async (req, res) => {
     profile_image: pathImage
   });
 
- 
-  const result = await AppDataSource.getRepository(User).save(user); 
+
+  const result = await AppDataSource.getRepository(User).save(user);
   return res.send(result);
 });
+
+app.post("/login", async (req, res) => {
+
+  const { sus_code, password } = req.body;
+
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    sus_code: sus_code,
+  });
+
+
+  const authentic = bcrypt.compareSync(password, user?.password!);
+
+  if (authentic) {
+    const token = jwt.sign({
+      sus_code: user?.sus_code,
+      CPF: user?.CPF,
+    }, "wel1ing7", {
+      expiresIn: "1h"
+    });
+
+    return res.json(token);
+  }
+
+  return res.send("falha");
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
